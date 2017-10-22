af4f68fe0680da4638b13b25f6c3a9813b7fd871
Kristijan Iliev <kikiiliev@yahoo.com>
2017-10-22 12:17:35 +0300

    initial setup for mockito tutorial

---
+++ src/com/mockito/tutorial/order/dao/OrderDAO.java
@@ -0,0 +1,16 @@
+  package com.mockito.tutorial.order.dao;
+
+import java.sql.SQLException;
+
+import com.mockito.tutorial.order.dto.Order;
+
+public interface OrderDAO {
+
+	int create(Order order) throws SQLException;
+	
+	Order read(int id) throws SQLException;
+	
+	int update(Order order) throws SQLException;
+	
+	int delete(int id) throws SQLException;
+}
---
+++ src/com/mockito/tutorial/order/dao/OrderDAOImpl.java
@@ -0,0 +1,33 @@
+package com.mockito.tutorial.order.dao;
+
+import java.sql.SQLException;
+
+import com.mockito.tutorial.order.dto.Order;
+
+public class OrderDAOImpl implements OrderDAO {
+
+	@Override
+	public int create(Order order) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Order read(int id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(Order order) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int delete(int id) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
