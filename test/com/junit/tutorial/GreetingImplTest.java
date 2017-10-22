3704ecfb57ceb7d3c354138411362b72b9179098
Kristijan Iliev <kikiiliev@yahoo.com>
2017-10-22 11:55:23 +0300

    junit tests for assertions and exceptions

--- test/com/junit/tutorial/GreetingImplTest.java
+++ test/com/junit/tutorial/GreetingImplTest.java
@@ -1,14 +1,50 @@
 package com.junit.tutorial;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
-class GreetingImplTest {
+public class GreetingImplTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private Greeting greeting;
+	@Before
+	public void setup() {
+		System.out.println("Setup");
+		greeting = new GreetingImpl();
 	}
 
+	@Test
+	public void greetShouldReturnAValidOutput() {
+		System.out.println("greetShouldReturnAValidOutput");
+		String result = greeting.greet("Junit");
+
+		assertNotNull(result);
+		assertEquals("Hello Junit", result);
+	}
+
+//	@Test(expected = IllegalArgumentException.class)
+//	public void greetShouldThrowAnException_For_NameIsNull() {
+//		Greeting greeting = new GreetingImpl();
+//		greeting.greet(null);
+//	}
+//
+//	@Test(expected = IllegalArgumentException.class)
+//	public void greetShouldThrowAnException_For_NameIsBlank() {
+//		Greeting greeting = new GreetingImpl();
+//		greeting.greet("");
+//	}
+
+	@Test(expected = ArithmeticException.class)
+	public void testDivisionWithException() {
+		System.out.println("testDivisionWithException");
+		int i = 1 / 0;
+	}
+	
+	@After
+	public void cleanUp() {
+		System.out.println("cleanUp");
+	}
 }
